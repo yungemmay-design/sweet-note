856f2eb9dfe2d37098f2edb7c173bea3bc0ed227
yungemmay-design <yungemmay@gmail.com>
2025-08-26 14:51:40 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+// script.js
+function createHeart() {
+  const heart = document.createElement("div");
+  heart.classList.add("heart");
+  heart.innerHTML = "❤️";
+  heart.style.left = Math.random() * 100 + "vw";
+  heart.style.animationDuration = (Math.random() * 3 + 2) + "s";
+  document.body.appendChild(heart);
+
+  setTimeout(() => {
+    heart.remove();
+  }, 5000);
+}
+
+setInterval(createHeart, 500);
